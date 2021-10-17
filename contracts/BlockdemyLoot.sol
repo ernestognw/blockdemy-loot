d31681e2c766ee8c4e7186763e4c40ef0088cc16
ernestognw <ernestognw@gmail.com>
2021-10-17 11:57:56 -0500

    Removed console

--- contracts/BlockdemyLoot.sol
+++ contracts/BlockdemyLoot.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "./BlockdemyLootAttributes.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
